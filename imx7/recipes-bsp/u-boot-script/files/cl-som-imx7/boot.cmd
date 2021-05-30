3008685ff63d5cd4d449c7c8f07ea00347ffadfd
vraevsky <valentin@compulab.co.il>
2021-05-30 16:06:19 +0300

    Update boot.cmd

--- imx7/recipes-bsp/u-boot-script/files/cl-som-imx7/boot.cmd
+++ imx7/recipes-bsp/u-boot-script/files/cl-som-imx7/boot.cmd
@@ -1,5 +1,5 @@
 run mender_setup
-setenv bootargs 'console=${console},${baudrate} root=${mender_kernel_root} rootwait rw'
+setenv bootargs "console=${console},${baudrate} root=${mender_kernel_root} rootwait rw"
 mmc dev ${mender_uboot_dev}
 load ${mender_uboot_root} ${loadaddr} /boot/${kernel}
 load ${mender_uboot_root} ${fdtaddr} /boot/${fdtfile}
