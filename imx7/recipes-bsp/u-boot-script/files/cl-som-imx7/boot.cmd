e0064d21a6fcdf7322e8ca1dbdcdbe78a5ea932c
Valentin Raevsky <valentin@compulab.co.il>
2021-11-09 15:40:57 +0200

    recipes-bsp: u-boot-script: Update the imx7 script
    Update the imx7 script:
    1) Evaluate boot/auto file & start auto installer if exists.
    2) Update the spi flash bootloader if possible.
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- imx7/recipes-bsp/u-boot-script/files/cl-som-imx7/boot.cmd
+++ imx7/recipes-bsp/u-boot-script/files/cl-som-imx7/boot.cmd
@@ -1,5 +1,11 @@
+# Try updating the u-boot 1-st
+load mmc 0:2 ${loadaddr} boot/boot.update.scr && source ${loadaddr}
+
 run mender_setup
+
 setenv bootargs "console=${console},${baudrate} root=${mender_kernel_root} rootwait rw"
+load mmc 0:2 ${loadaddr} boot/auto && setenv bootargs ${bootargs} init=/usr/local/bin/cl-init
+
 mmc dev ${mender_uboot_dev}
 load ${mender_uboot_root} ${loadaddr} /boot/${kernel}
 load ${mender_uboot_root} ${fdtaddr} /boot/${fdtfile}
